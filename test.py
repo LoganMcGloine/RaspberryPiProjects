9dfdfe2d53b73f7d7dd8a662b6aa10175a2369ab
Logan McGloine <136609167+LoganMcGloine@users.noreply.github.com>
2025-02-15 22:21:28 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,28 @@
+from gpiozero import LED, Button, Buzzer, MotionSensor, DistanceSensor
+from time import sleep
+import datetime
+
+
+led = LED(17)
+button = Button(2)
+buzzer = Buzzer(14)
+#pir = MotionSensor(15)
+ultrasonic = DistanceSensor(echo=15, trigger=18)
+
+#while True:
+#	print(ultrasonic.distance)
+
+
+led_on = False
+buzzer.on()
+sleep(1)
+buzzer.off()
+while True:
+#	pir.wait_for_motion()
+	button.wait_for_press()
+	led.on()
+	buzzer.on()
+	sleep(1)
+	led.off()
+	buzzer.off()
+#	print("movement detected at ", datetime.datetime.now().strftime("%H:%M:%S"))
